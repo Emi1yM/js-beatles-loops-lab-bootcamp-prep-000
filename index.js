363bf4892f36b0819548b0c614618e19cce0a90a
upbeat-scope-9442 <mle.jo.11@gmail.com>
2019-07-07 23:56:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// add solution here
+var musiciansArr = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"]
+var instrumentsArr = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"]
+
+function theBeatlesPlay(musiciansArr, instrumentsArr){
+  var array = [];
+for (let i = 0; i<3; i++)
+  array.push("${musiciansArr[i]} plays ${instrumentsArr[i]}");
+}
+  return array;
+}
