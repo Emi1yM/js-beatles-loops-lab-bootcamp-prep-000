c9499c42d2b3994946a100e7f79887ddb93b4703
upbeat-scope-9442 <mle.jo.11@gmail.com>
2019-07-08 00:01:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
-var musiciansArr = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"]
-var instrumentsArr = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"]
+var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 
-function theBeatlesPlay(musiciansArr, instrumentsArr){
+function theBeatlesPlay(musicians, instruments){
   var array = [];
-for (let i = 0; i<3; i++)
-  array.push("${musiciansArr[i]} plays ${instrumentsArr[i]}");
+for (let i = 0; i < 4; i++)
+  array.push("${musicians[0]} plays ${instruments[0]}");
 }
   return array;
 }
 
 
+theBeatlesPlay(musicians, instruments);
+
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+
+function johnLennonFacts(facts) {
+  while () {
+    facts[n] + "!!!";
+  }
+  return facts;  
+}
+
+function iLoveTheBeatles(x) {
+  var array = [];
+  do {
+    x = array.push("I love the Beatles!");
+    x++;
+  } while (x < 15); {
+    
+  }
+  return array;
+}
+
+iLoveTheBeatles(12);*/
